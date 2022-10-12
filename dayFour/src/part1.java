bacf37b7b2deeec3a2f1f73d40be121fe4e2cbff
GabriMac <gabri1998gabri@gmail.com>
2022-10-13 00:29:20 +0200

    Trying to fix day4

--- dayFour/src/part1.java
+++ dayFour/src/part1.java
@@ -4,6 +4,13 @@ import java.util.*;
 
 public class part1 {
 
+    class Pair
+    {
+        public static <T, U> Map.Entry<T, U> of(T first, U second) {
+            return new AbstractMap.SimpleEntry<>(first, second);
+        }
+    }
+
     public static List<Integer> parseBingoNumbers(String numbers) {
         List<Integer> bingoNumbers = new LinkedList<>();
         Integer sum = Character.getNumericValue(numbers.charAt(0))*10;
@@ -26,26 +33,89 @@ public class part1 {
         return bingoNumbers;
     }
 
+    private static void readBoards(List<List<List<Map.Entry<Integer, Boolean>>>> boards, Scanner sc) {
+        while (sc.hasNextLine()){
+            List<List<Map.Entry<Integer, Boolean>>> board = new LinkedList<>();
+            for(int i = 0; i< 5; i++) {
+                List<Map.Entry<Integer, Boolean>> boardEntry = new ArrayList<>();
+                for (int j = 0; j < 5; j++)
+                    boardEntry.add(Pair.of(Integer.parseInt(sc.next()), false));
+
+                board.add(boardEntry);
+
+            }
+            boards.add(board);
+        }
+    }
+
+    public static void markBoard(List<List<Map.Entry<Integer, Boolean>>> board, Integer num){
+        for (List< Map.Entry<Integer,Boolean>> entryBoard : board){
+            for (Map.Entry<Integer,Boolean> entryNumber : entryBoard){
+                if(entryNumber.getKey().equals(num))
+                    entryNumber.setValue(true);
+            }
+        }
+    }
+
+    public static Integer checkWin(List<List< Map.Entry<Integer,Boolean>>> board){
+
+        Integer sum = 0;
+        Boolean win = false, line = true , col1 = true, col2 = true, col3 = true,
+                col4 = true, col5 = true;
+
+        for (List< Map.Entry<Integer,Boolean>> entryBoard : board){
+            for (int i = 0; i < entryBoard.size(); i++) {
+                Map.Entry<Integer, Boolean> entryNumber = entryBoard.get(i);
+                if (entryNumber.getValue().equals(false))
+                    line = false;
+                if (entryNumber.getValue().equals(false) && i==0)
+                    col1 = false;
+                else if (entryNumber.getValue().equals(false) && i==1)
+                    col2 = false;
+                else if (entryNumber.getValue().equals(false) && i==2)
+                    col3 = false;
+                else if (entryNumber.getValue().equals(false) && i==3)
+                    col4 = false;
+                else if (entryNumber.getValue().equals(false) && i==4)
+                    col5 = false;
+            }
+
+            if(line) win = true;
+            else    line = true;
+
+        }
+        if(col1 || col2 || col3 || col4 || col5) win = true;
+
+        if (win){
+            for (List< Map.Entry<Integer,Boolean>> entryBoard : board){
+                for (Map.Entry<Integer,Boolean> entryNumber : entryBoard){
+                    if(entryNumber.getValue().equals(false))
+                        sum+= entryNumber.getKey();
+                }
+            }
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("dayFour//src//input.txt");
         Scanner sc = new Scanner(file);
 
         List<Integer> bingoNumbers = parseBingoNumbers(sc.next());
+        List<List<List< Map.Entry<Integer,Boolean>>>> boards = new LinkedList<>();
 
-        List<Map<Integer,Map<Integer,Boolean>>> boards = new LinkedList<Map<Integer,Map<Integer,Boolean>>>();
+        readBoards(boards,sc);
 
-        while (sc.hasNextLine()){
-            HashMap<Integer, Map<Integer, Boolean>> board = new HashMap<>();
-            Map<Integer,Boolean> nestedBoard = new HashMap<Integer,Boolean>();
-            for(int i = 0; i< 5; i++)
-                for(int j = 0; j<5;j++){
-                    nestedBoard.put(Integer.parseInt(sc.next()),false);
-                    board.put(Integer.parseInt(sc.next()),nestedBoard);
+        for(Integer n : bingoNumbers){
+            for(List<List< Map.Entry<Integer,Boolean>>> board: boards){
+                markBoard(board,n);
+                if(checkWin(board)>0) {
+                    System.out.println(checkWin(board) * n);
+                    return;
                 }
-            boards.add(board);
+            }
         }
-
-        System.out.println("fi");
-
     }
+
 }
